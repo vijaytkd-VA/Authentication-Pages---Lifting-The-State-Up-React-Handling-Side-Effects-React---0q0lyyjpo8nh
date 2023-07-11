b820ecddf08fdb520bead5f16e9bf2d9d53fcc21
vijaytkd-VA <127473876+vijaytkd-VA@users.noreply.github.com>
2023-07-11 07:46:54 +0530

    Update Signin.js

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signIn } from '../api/auth';
 
+export const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-export const Signin = ()=>{
-    return(
-        <div id="signin-page">
-            <h1>Sign in page</h1>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signIn({ email, password });
+      // Handle successful sign-in, e.g., redirect to home page
+    } catch (error) {
+      setError('Wrong email or password');
+    }
+  };
 
-        </div>
-    )
-}
+  return (
+    <div id="signin-page">
+      <h1>Sign in page</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign in</button>
+      </form>
+      {error && <p className="error-txt">{error}</p>}
+    </div>
+  );
+};
