69638940f34c8aeccdfdbb6a80c9fccf7a12987a
vijaytkd-VA <127473876+vijaytkd-VA@users.noreply.github.com>
2023-07-11 07:49:16 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,44 @@
-import React from 'react';
-export const Signup = () =>{
-    return(
-        <div id="signin-page">
-            <h1>Sign Up page</h1>
+import React, { useState } from 'react';
+import { signUp } from '../api/auth';
 
-        </div>
-    )
-}
+export const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp({ email, password });
+      // Handle successful sign-up, e.g., redirect to home page
+    } catch (error) {
+      setError('Sign up failed');
+    }
+  };
+
+  return (
+    <div id="signup-page">
+      <h1>Sign Up page</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign up</button>
+      </form>
+      {error && <p className="error-txt">{error}</p>}
+    </div>
+  );
+};
