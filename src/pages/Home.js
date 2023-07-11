944aaae8dba13305decc5f094cd8279a890b94c2
vijaytkd-VA <127473876+vijaytkd-VA@users.noreply.github.com>
2023-07-11 07:46:34 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-export const Home = () =>{
-
-    return(
-        <div id="home-page">
-            Home page
-        </div>
-    )
-}
+export const Home = () => {
+  return <div id="home-page">Home page</div>;
+};
